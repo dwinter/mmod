72dccbf7eb3d5a42e6648ecaa3413a615419dc3a
David Winter <winda002@student.otago.ac.nz>
2012-04-03 20:37:58 +1200

    added Phi_st functions

--- R/Phi_st_Meirmans.R
+++ R/Phi_st_Meirmans.R
@@ -1,60 +1,52 @@
-#Internal function, used to get maximum possible distance for dataset
-max_between_dist <- function(pop_name, pop_freqs){
-    inter_dists <- sapply(pop_freqs[names(pop_freqs) != pop_name], 
-                      function(x) x * pop_freqs[pop_name])
-    return(sum(inter_dists))
+Phi_st_meirmans <- function(x){
+
+
+	add_max_distance <- function(x, index){
+	sum(sapply(x[-1*index], function(y) y*x[index]))
+	}	
+	
+	amova_st <- function(dm, dropped){
+	
+	if(is.null(dropped)){
+		pops <- x@pop
+	}
+	else {
+		pops <- droplevels(x@pop[-1 * dropped])
+	}
+
+	#values used multiple times
+	sq_distance <- dm^2
+	pop.freqs <- table(pops)
+	n <- dim(dm)[1]
+	df <- length(unique(pops)) - 1 
+	within_dists <- sapply(unique(pops), function(p) sum(sq_distance[outer(pops==p, pops==p) == 1] ))
+	ncoef <- (n - sum(pop.freqs^2)/n)/df
+
+	
+	#normal old AMOVA
+	SSD_total <- sum(sq_distance/(2 * n))
+	SSD_within <- sum(within_dists / (as.numeric(pop.freqs) *2))
+	SSD_among <- SSD_total - SSD_within
+  
+	MSD_total <-SSD_total / (n-1)
+	MSD_among <- SSD_among / df
+	MSD_within <- SSD_within / ((n -1 ) - df)
+	sigma2a <- (MSD_among - MSD_within)/ncoef
+	phi <- sigma2a /(sigma2a + MSD_within)
+	
+	#Now AMOVA to the max
+	max_between <- sapply(unique(pops), max_between_dist, pop.freqs)
+	max_SSD_total <- sum(within_dists + max_between) / (2*n)
+	max_MSD_among <- (max_SSD_total - SSD_within) / df
+	sigma2_prime <- (max_MSD_among - MSD_within)/ncoef
+	phi_max = sigma2a_prime /(sigma2a_prime + MSD_within)
+	phi_prime <- phi/phi_max
+	
+	return(phi_prime)
+	}
+
+	global <- with(dist.codom(x), amova_st(distances, dropped))
+	loci <- sapply(dist.codom(x, global=FALSE), function(d) with(d, amova_st(distances, dropped)))
+	return(list(per.locus=loci, global=global))
 }
 
-Phi_st_Meirmans <- function(x){
-    
-    amova_st <- function(dm){    
-    
-      if(any(is.na(locus@tab))){
-        warning(paste("Ommitting NAs from locus", locus@loc.names))
-        locus@tab <- na.omit(locus@tab)
-        locus@pop <- droplevels(locus@pop[-1 * attr(locus@tab, "na.action")])
-      }
-      
-      dm <- dist.codom(locus)^2
-      n <- dim(dm)[1]
-      pop.freqs <- table(pops)
-      
-      #values we will need multiple times  
-      df <- length(unique(pops)) - 1
-      # within populaton distances for each population
-      # more readble to do this with melt/reshape of l@tab but 
-      # avoid dependency
-      within_dists <- sapply(unique(pops), function(p) 
-                      sum(dm[outer(pops==p, pops==p) == 1] ))
-      ncoef <- (n - sum(pop.freqs^2)/n)/df
-
-      #Normal old AMOVA
-      SSD_total <- sum(dm/(2 * n))
-      SSD_within <- sum(within_dists / (as.numeric(pop.freqs) * 2))
-      SSD_among <- SSD_total - SSD_within
-      
-      MSD_total <-SSD_total / (n-1)
-      MSD_among <- SSD_among / df
-      MSD_within <- SSD_within / ((n -1 ) - df)
-      sigma2 <- (MSD_among - MSD_within)/ncoef
-      phi = sigma2/(sigma2  + MSD_within)
-      
-      #Now compared to maximum value of SSD
-      max_between <- sapply(unique(pops), max_between_dist, pop.freqs)
-      max_SSD_total <- sum(within_dists + max_between) / (2*n)
-      SSD_among_prime <- max_SSD_total - SSD_within
-      MSD_among_prime <- SSD_among_prime / df
-      sigma2_prime <- (MSD_among_prime - MSD_within)/ncoef
-      phi_max = sigma2_prime/(sigma2_prime  + MSD_within)
-      phi_prime = phi/phi_max
-      
-      return(c(SSD=SSD_among, SSD_prime = SSD_among_prime, df=df, 
-               phi_st=phi, phi_prime_st=phi_prime))
-     }
-     phi_loci <- sapply(seploc(x), amova_st))
-     phi_global <- mean(phi_loci[,5])
-     return(loci=phi_loci, global=phi_global)
-     
- }
- 
- 
--- R/dist.codom.R
+++ R/dist.codom.R
@@ -7,6 +7,10 @@
 #' 
 #'
 #' @param x genind object (from package adegenet)
+#' @param matrix Boolean return matrix (not dist object)
+#' @param seplocs Boolean if TRUE, return a list of matrices for each locus,
+#' if FALSE a single global estimate based on all loci
+#' @param na.rm Boolean if TRUE remove individuals with NAs
 #'
 #' @return either a list of distance matrices, one for each locus or a single 
 #' matrix containing the mean distance between individuals across all loci
@@ -20,29 +24,56 @@
 #' data(nancycats)
 #' dist_codom(nancycats[,1])
 
-dist.codom <- function(g, matrix=TRUE, seplocs=TRUE){
-  per.locus <- function(l){
-      n.inds <- dim(l@tab)[1]
-      allP <- combn(1:n.inds, 2)
-      pair <- function(x, y){
-        return( sum( abs(l@tab[x,]- l@tab[y,]) ) / 2 )
-      }
-      res <- sapply(1:dim(allP)[2], function(i) pair(allP[,i][1], allP[,i][2]))
-      #trick to turn vector into distance matrix
-      attributes(res) <- attributes(dist(1:n.inds))
-      if(!matrix){
-	    return(res)
-      }
-      else return(as.matrix(res))
+
+dist.codom <- function(x, matrix=TRUE, global=TRUE, na.rm=TRUE){
+  
+  pair <- function(l, x, y){
+  #find distance between individuls x and y for locus l
+    return( sum( abs(l@tab[x,]- l@tab[y,]) ) / 2 )
   }
-  loci <- lapply(seploc(g), per.locus)  
-  if (!seplocs){
-    if(any(is.na(g@tab))){
-        warning("Ommitting individuals with NAs")
-        no_nas <- apply(g@tab, 1, function(x) !any(is.na(x)) )
-        loci <- lapply(loci, function(x) x[no_nas,no_nas])
-    }
-    return( Reduce("+", loci) / length(loci) )
-  }  
-  else return(loci)
+  
+  per.loc <- function(l){
+	if(na.rm){ 
+	  l@tab <- na.omit(l@tab)
+	  dropped <- attr(l@tab, "na.action") 
+	}
+	n.inds <- dim(l@tab)[1]
+	allP <- combn(1:n.inds, 2)
+	res <- sapply(1:dim(allP)[2], function(i) pair(l, allP[,i][1], allP[,i][2]))
+	#trick to turn vector into distance matrix
+	attributes(res) <- attributes(dist(1:n.inds))	
+	if(matrix) {
+		res <- as.matrix(res)
+		}
+	if(na.rm){
+		return(list(distances=res, dropped=dropped))
+		}
+	else return(res)		
+	}
+
+  
+	if(global){
+		has_nas <- FALSE
+		if(any(is.na(x@tab))){
+		# if we want a global estimate make new genind with out NAs
+		# (just chopping out NAs from x@tab won't work)
+			has_nas  <- TRUE
+			warning("removing all individuals with NAs for global estimate")
+			n_tab <- na.omit(x@tab)
+			dropped <- attr(n_tab, "na.action")
+			n_pop <- droplevels(x@pop[ -1 * dropped])
+			x <- genind(tab=n_tab, pop=n_pop)
+		}	
+		dists <- lapply(seploc(x), per.loc)
+		xbar <- Reduce("+", lapply(dists, function(x) x$dist))  / length(dists)
+		if(has_nas){
+			return(list(distances=xbar, dropped=dropped))
+		}
+		else return(mean)
+	}
+	
+	else{
+		dists <- lapply(seploc(x), per.loc)	
+		return(dists)
+	}
 }
