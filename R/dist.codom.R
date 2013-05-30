e53157597f94ea250c27407e8968fbafa807abb9
David Winter <winda002@student.otago.ac.nz>
2013-05-30 23:23:15 +1200

    non-vectory dists

--- R/dist.codom.R
+++ R/dist.codom.R
@@ -54,7 +54,26 @@ dist.codom <- function(x, matrix=TRUE, global=TRUE, na.rm=TRUE){
     }
   
 	if(global){
-		has_nas <- FALSE
+
+
+
+dist.codom.slow <- function(x){
+        all_loci <- seploc(x)
+        nloc <- length(all_loci)
+        n <- length(x@ind.names)
+        dm <- matrix(0,n,n)
+        missing <- matrix(0,n,n)
+        for(i in 1:nloc){
+            dists <- as.matrix(dist(all_loci[[i]]@tab, "manhattan" )/x@ploidy)
+            missing <- missing + ifelse(is.na(dists),1,0)
+            dists[is.na(dists)] <- 0
+            dm <- dm + dists
+        }
+        res <- dm/(nloc-missing)
+        return(res)
+}
+
+
 		if(any(is.na(x@tab))){
 		# if we want a global estimate make new genind with out NAs
 		# (just chopping out NAs from x@tab won't work)
