bece1c488f925b355653e84e6e144a8f51585e0f
David Winter <david.winter@gmail.com>
2015-06-23 21:36:47 -0700

    start on #1, hsht with new acessor fxns

--- R/HsHt.R
+++ R/HsHt.R
@@ -7,11 +7,14 @@
 # function is not exported (if someone has a use for it I can change this).
 
 HsHt <- function(x){
+    #start by dropping missing for this locus
+    x <- x[apply(x@tab, 1, function(i) any(i > 0)),]
+    n_by_pop <-  table(pop(x))
     pops <- pop(x)
-    n_by_pop <- by(x@tab, pops, sum, na.rm=TRUE)
-    n <- sum(n_by_pop > 0)
+    n <- nPop(x)
     harmN <- harmonic_mean(n_by_pop[n_by_pop >0])
-    a <- apply(x@tab,2,function(row) tapply(row, pops, mean, na.rm=TRUE))
+    a <- apply(apply(x@tab, 2, "/", ploidy(x)), 2, function(L) 
+                   tapply(L, pops, mean, na.rm=TRUE))
     HpS <- sum(1 - apply(a^2, 1, sum, na.rm=TRUE)) / n
     Hs_est <- (2*harmN/(2*harmN-1))*HpS
     HpT <- 1 - sum(apply(a,2,mean, na.rm=TRUE)^2)
