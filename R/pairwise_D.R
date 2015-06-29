183472292055e66b98cba46bb61c646687e02393
David Winter <david.winter@gmail.com>
2015-06-28 20:02:42 -0700

    Pairwise fxns get overhaul for #3
    First, following advice from @zkamvar now use the `[pop=x]` accessor
    to hande downsampling which is both cleaner and saves some time.
    
    Second, DRYed out the pairwise functions by writing more generic case
    that each specific one can use

--- R/pairwise_D.R
+++ R/pairwise_D.R
@@ -1,3 +1,19 @@
+#Let's DRY out the pairwise functions but writing a general case that 
+#all others (below) can make use of.
+
+pairwise_fxn <- function(x, linearized=FALSE, FXN, VAL, ...){
+    pops <- seppop(x)
+    n.pops <- length(pops)
+    #all combinations 
+    res <- utils::combn(1:n.pops, 2, function(p) FXN(x[pop=p], ...)[[VAL]])
+    attributes(res) <- list(class="dist", Diag=FALSE, Upper=FALSE, 
+                            Labels=popNames(x),Size=n.pops)
+    if(linearized){
+        return(res/(1-res))
+    }
+    res
+}
+
 #' Calculates pairwise values of Jost's D
 #'
 #' This function calculates Jost's D, a measure of genetic
@@ -19,22 +35,57 @@
 #' @family D
 
 pairwise_D <- function(x, linearized=FALSE, hsht_mean="arithmetic") {
-  pops <- seppop(x)
-  n.pops <- length(pops)
-  #all combinations 
-  allP <- utils::combn(1:n.pops, 2)
-  # calculate tfh statistic
-  pair <- function(index.a,index.b){
-    a <- pops[[index.a]]
-    b <- pops[[index.b]]
-    temp <- repool(a,b)
-    return(D_Jost(temp, hsht_mean=hsht_mean)$global.het)
-    }
-  res <- sapply(1:dim(allP)[2], function(i) pair(allP[,i][1], allP[,i][2]))
-  attributes(res) <- list(class="dist", Diag=FALSE, Upper=FALSE, 
-                          Labels=popNames(x),Size=n.pops)
-  if(linearized){
-     return(res/(1-res))
-     }
-  else(return(res))
+    pairwise_fxn(x, linearized, D_Jost, "global.het", hsht_mean=hsht_mean)
 }
+
+#' Calculates pairwise values of Hedrick's G'st
+#'
+#' This function calculates Hedrick's G'st, a measure of genetic
+#' differentiation, between all combinations of populaitons
+#' in a genind object.
+#'
+#' @return A distance matrix with between-population values of G''st
+#' @param x genind object (from package adegenet)
+#' @param linearized logical, if TRUE will turned linearized G'st (1/()1-G'st))
+#' @export
+#' @examples
+#' 
+#' data(nancycats)
+#' pairwise_Gst_Hedrick(nancycats[1:26,])
+#' @references
+#'  Hedrick, PW. (2005), A Standardized Genetic Differentiation Measure. Evolution 59: 1633-1638. 
+#' @family pairwise
+#' @family Hedrick
+
+pairwise_Gst_Hedrick<- function(x, linearized=FALSE) {
+    pairwise_fxn(x, linearized, Gst_Hedrick, "global")
+}
+
+#' Calculates pairwise values of Nei's Gst
+#'
+#' This function calculates Nei's Gst, a measure of genetic
+#' differentiation, between all combinations of populaitons
+#' in a genind object.
+#'
+#' @return dist A distance matrix with between-population values of Gst
+#' @param x genind object (from package adegenet)
+#' @param linearized logical, if TRUE will turned linearized Gst (1/(1-Gst))
+#' @export
+#' @examples
+#' 
+#' data(nancycats)
+#' pairwise_Gst_Nei(nancycats[1:26,])
+#' @references
+#'  Nei M. (1973) Analysis of gene diversity in subdivided populations. PNAS: 3321-3323. 
+#' @references
+#'  Nei M, Chesser RK. (1983). Estimation of fixation indices and gene diversities. Annals of Human Genetics. 47: 253-259.
+#' @family pairwise
+#' @family Nei
+
+
+pairwise_Gst_Nei <- function(x, linearized=FALSE) {
+    pairwise_fxn(x, linearized, Gst_Nei, "global")
+}
+
+
+
